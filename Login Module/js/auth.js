12b0441e1c41eac9f9a6f39403d895691ddf40be
Mano Vishnu <geekymano@gmail.com>
2018-12-01 00:14:39 +0530

    Login Module added

---
+++ Login Module/js/auth.js
@@ -0,0 +1,84 @@
+var user;
+var googleprovider = new firebase.auth.GoogleAuthProvider();
+var githubprovider = new firebase.auth.GithubAuthProvider();
+var twitterprovider = new firebase.auth.TwitterAuthProvider();
+
+
+
+function googlesignin() {
+    firebase.auth().signInWithPopup(googleprovider).then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        user = result.user;
+        // ...
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+}
+
+
+
+
+function gituhbsignin() {
+    firebase.auth().signInWithPopup(githubprovider).then(function(result) {
+        // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        user = result.user;
+        // ...
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+}
+
+
+function twittersignin() {
+    firebase.auth().signInWithPopup(twitterprovider).then(function(result) {
+        // This gives you a the Twitter OAuth 1.0 Access Token and Secret.
+        // You can use these server side with your app's credentials to access the Twitter API.
+        var token = result.credential.accessToken;
+        var secret = result.credential.secret;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+}
+
+
+
+//Email Login
+function emailLogin() {
+  const email = document.getElementById('txtemail').value;
+  const password = document.getElementById('txtpassword').value;
+  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  window.alert('Error:'+errorMessage);
+  // ...
+}); 
+}
