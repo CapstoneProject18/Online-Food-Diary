91f80c0b902f0df2734a7080e19c480814bdb5b2
Mano Vishnu <geekymano@gmail.com>
2018-12-06 16:01:57 +0530

    Feature Added

--- Login Module/js/firebase-config.js
+++ Login Module/js/firebase-config.js
@@ -1,10 +1,10 @@
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCbyObWiaXuqYcVk7H5QAsANoYY1xnYMDg",
-    authDomain: "foodtel-857f2.firebaseapp.com",
-    databaseURL: "https://foodtel-857f2.firebaseio.com",
-    projectId: "foodtel-857f2",
-    storageBucket: "foodtel-857f2.appspot.com",
-    messagingSenderId: "533408782485"
-  };
-  firebase.initializeApp(config);
+ // Initialize Firebase
+ var config = {
+  apiKey: "AIzaSyCbyObWiaXuqYcVk7H5QAsANoYY1xnYMDg",
+  authDomain: "foodtel-857f2.firebaseapp.com",
+  databaseURL: "https://foodtel-857f2.firebaseio.com",
+  projectId: "foodtel-857f2",
+  storageBucket: "foodtel-857f2.appspot.com",
+  messagingSenderId: "533408782485"
+};
+firebase.initializeApp(config);
--- main.js
+++ main.js
@@ -20,132 +20,148 @@ foodieApp.config(function ($routeProvider) {    //$routeProvider is an object de
 
 foodieApp.controller('loginController',function($scope) {
 })
-var hoteldata = JSON.parse(localStorage.getItem('user'));
-	console.log(hoteldata);
+
+var getmyString = localStorage.getItem('user');
+var hoteldata = JSON.parse(getmyString);
+	console.log(hoteldata, typeof(getmyString),typeof(hoteldata));
+	var shitjson={ 
+                        id: 9,
+						name: 'Chessiano Pizza',
+						address: 'Opposite Pantaloons, Senapati Bapat Road, Pune',
+						location: 'Senapati Bapat Road',
+						category: 'Quick Bites',
+						vote: '3.3',
+						noofvotes: '460 Votes',
+						cuisines: 'Pizza',
+						url: 'Chessiano Pizza',
+						cost: '1800',
+						hours: '11 AM to 1 AM (Mon-Sun)',
+						email: 'uncleschineese@gmail.com',
+                        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSk_TLf0yvTv4vaYBT93zC0a-HX7O2c7DzybqlMvpDbSfQxPXrxtA'
+            }
 foodieApp.controller('mainController',function($scope) {
 	$scope.restaurants = [{
-													id: 1,
-													name: "Uncle's Chinese",
-													address: '3 & 5, A/10, Meera Garden Society, Lane 7, Off North Mai....',
-													location: 'Koregaon  Park',
-													category: 'Casual Dining, Bar',
-													vote: '4.0',
-													noofvotes: '400 Votes',
-													cuisines: 'Chinese, Thai',
-													url: 'Uncle"s Chinese',
-													cost: '700',
-													email: 'uncleschineese@gmail.com',
-													hours: '11:30 AM to 11 PM (Mon-Sun)',
-													bestDish: {
-																			name: 'Corn Pizza',
-																			image: 'http://noblepig.com/images/2016/06/Avocado-and-Three-Bean-Salad-is-perfect-for-a-summertime-barbecue-side-dish.JPG'
-																		},
-													image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3f-_yzrJq4F042m-9qIfSr_Ye69ncgDK9aZxtjwOph7ExEGZcdQ'
-												},
-												{
-													id: 2,
-													name: 'Idlicious',
-													address: 'Cosmos Garden, Plot 16 and 17, P K Shroff Marg, Pancard Clubs Road, Baner, Pune',
-													location: 'Baner',
-													category: 'Quick Bites, Bar',
-													vote: '4.2',
-													noofvotes: '400 Votes',
-													cuisines: 'South Indian',
-													url: 'Idlicious',
-													cost: '250',
-													hours: '8:30 AM to 2:30 PM (Mon-Sat)',
-													email: 'uncleschineese@gmail.com',
-													image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_dY-UiYvGUz5HSy3ATyR-2mBU5YD2L5QMj4dVu7lIAIHqAXSjAQ'
-											},
-											{
-												id: 3,
-												name: 'Cream California',
-												address: '917/22, Opposite Hotel Roopali, FC Road, Pune',
-												location: 'FC Road',
-												category: 'Dessert Parlor',
-												vote: '4.2',
-												noofvotes: '260 Votes',
-												cuisines: 'Ice Cream, Desserts',
-												url: 'Cream California',
-												cost: '1000',
-												hours: '9 AM to 11:30 PM (Mon-Sun)',
-												email: 'uncleschineese@gmail.com',
-												image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJ9vqv4_9lvEhauSuO-_MdBG3Xyyd1-NI9rRLBnSQEOuy_E2kJjQ'
-											},
-											{
-												id: 4,
-												name: 'Hotel Decent',
-												address: '1, Shyamal Apartments, Next to Ozone, ITI Road, Aundh, New Delhi',
-												location: 'Aundh',
-												category: 'Casual Dining',
-												vote: '3.9',
-												noofvotes: '410 Votes',
-												cuisines: 'North Indian, Street Food',
-												url: 'Taareef',
-												cost: '1200',
-												hours: '11:30 AM to 11 PM (Mon-Sun)',
-												email: 'uncleschineese@gmail.com',
-												image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2tekOd1nc3in62aFK5u7n48qXbtFLNX6o5IyguGDWKItK4wE5'
-											},
-											{
-												id: 5,
-												name: 'Farzi Cafe',
-												address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
-												location: 'Connaught Place',
-												category: 'Casual Dining, Bar',
-												vote: '4.2',
-												cuisines: 'Modern Indian',
-												cost: '2600',
-												hours: '12 Noon to 1 AM (Mon-Sun)',
-												email: 'uncleschineese@gmail.com',
-												image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
-											},
-											{
-												id: 6,
-												name: 'McDonals',
-												address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
-												location: 'Connaught Place',
-												category: 'Casual Dining, Bar',
-												vote: '4.2',
-												cuisines: 'Modern Indian',
-												cost: '2200',
-												hours: '12 Noon to 1 AM (Mon-Sun)',
-												email: 'uncleschineese@gmail.com',
-												image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
-											},
-											{
-												id: 7,
-												name: 'Burgrill',
-												address: 'DSS 310, Sector 9, Panchkula',
-												location: 'Sector 9, Panchkula',
-												category: 'Quick Bites, Bar',
-												vote: '4.0',
-												noofvotes: '360 Votes',
-												cuisines: 'Burger,Fast Food,Beverages',
-												url: 'Burgrill',
-												cost: '1500',
-												hours: '12 Noon to 11 PM (Mon-Sun)',
-												email: 'uncleschineese@gmail.com',
-												image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUL31oTqn07dDzIPDaE32-C8wKPenINJXMPEf82g1jQY0PF5PL'
-											},
-											{
-												id: 8,
-												name: 'Chessiano Pizza',
-												address: 'Opposite Pantaloons, Senapati Bapat Road, Pune',
-												location: 'Senapati Bapat Road',
-												category: 'Quick Bites',
-												vote: '3.3',
-												noofvotes: '460 Votes',
-												cuisines: 'Pizza',
-												url: 'Chessiano Pizza',
-												cost: '1800',
-												hours: '11 AM to 1 AM (Mon-Sun)',
-												email: 'uncleschineese@gmail.com',
-												image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSk_TLf0yvTv4vaYBT93zC0a-HX7O2c7DzybqlMvpDbSfQxPXrxtA'
-											},
-											hoteldata
-										]
-						});
+		id: 1,
+		name: 'Uncle"s Chinese',
+		address: '3 & 5, A/10, Meera Garden Society, Lane 7, Off North Mai....',
+		location: 'Koregaon  Park',
+		category: 'Casual Dining, Bar',
+		vote: '4.0',
+		noofvotes: '400 Votes',
+		cuisines: 'Chinese, Thai',
+		url: 'Uncle"s Chinese',
+		cost: '700',
+		hours: '11:30 AM to 11 PM (Mon-Sun)',
+		email: 'uncleschineese@gmail.com',
+		bestDish: {
+								name: 'Corn Pizza',
+								image: 'http://noblepig.com/images/2016/06/Avocado-and-Three-Bean-Salad-is-perfect-for-a-summertime-barbecue-side-dish.JPG'
+							},
+		image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3f-_yzrJq4F042m-9qIfSr_Ye69ncgDK9aZxtjwOph7ExEGZcdQ'
+	},
+	{
+		id: 2,
+		name: 'Idlicious',
+		address: 'Cosmos Garden, Plot 16 and 17, P K Shroff Marg, Pancard Clubs Road, Baner, Pune',
+		location: 'Baner',
+		category: 'Quick Bites, Bar',
+		vote: '4.2',
+		noofvotes: '400 Votes',
+		cuisines: 'South Indian',
+		url: 'Idlicious',
+		cost: '250',
+		hours: '8:30 AM to 2:30 PM (Mon-Sat)',
+		email: 'idlicious@gmail.com',
+		image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
+},
+{
+	id: 3,
+	name: 'Cream California',
+	address: '917/22, Opposite Hotel Roopali, FC Road, Pune',
+	location: 'FC Road',
+	category: 'Dessert Parlor',
+	vote: '4.2',
+	noofvotes: '260 Votes',
+	cuisines: 'Ice Cream, Desserts',
+	url: 'Cream California',
+	cost: '1000',
+	hours: '9 AM to 11:30 PM (Mon-Sun)',
+	email: 'creamcalifornia@gmail.com',
+	image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJ9vqv4_9lvEhauSuO-_MdBG3Xyyd1-NI9rRLBnSQEOuy_E2kJjQ'
+},
+{
+	id: 4,
+	name: 'Hotel Decent',
+	address: '1, Shyamal Apartments, Next to Ozone, ITI Road, Aundh, New Delhi',
+	location: 'Aundh',
+	category: 'Dessert Parlor',
+	vote: '3.9',
+	noofvotes: '410 Votes',
+	cuisines: 'North Indian, Street Food',
+	url: 'Taareef',
+	cost: '1200',
+	hours: '11:30 AM to 11 PM (Mon-Sun)',
+	email: 'hoteldecent@gmail.com',
+	image: 'https://www.goomo.com/cmsimages/default-source/home-page/hotel_new/hotel_clarks.jpg'
+},
+{
+	id: 5,
+	name: 'Farzi Cafe',
+	address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
+	location: 'Connaught Place',
+	category: 'Casual Dining, Bar',
+	vote: '4.2',
+	cuisines: 'Modern Indian',
+	cost: '2600',
+	hours: '12 Noon to 1 AM (Mon-Sun)',
+	email: 'farzicafe@gmail.com',
+	image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
+},
+{
+	id: 6,
+	name: 'McDonals',
+	address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
+	location: 'Connaught Place',
+	category: 'Casual Dining, Bar',
+	vote: '4.2',
+	cuisines: 'Modern Indian',
+	cost: '2200',
+	hours: '12 Noon to 1 AM (Mon-Sun)',
+	email: 'mcdonalds@gmail.com',
+	image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
+},
+{
+	id: 7,
+	name: 'Burgrill',
+	address: 'DSS 310, Sector 9, Panchkula',
+	location: 'Sector 9, Panchkula',
+	category: 'Quick Bites, Bar',
+	vote: '4.0',
+	noofvotes: '360 Votes',
+	cuisines: 'Burger,Fast Food,Beverages',
+	url: 'Burgrill',
+	cost: '1500',
+	hours: '12 Noon to 11 PM (Mon-Sun)',
+	email: 'burgrill@gmail.com',
+	image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUL31oTqn07dDzIPDaE32-C8wKPenINJXMPEf82g1jQY0PF5PL'
+},
+{
+	id: 8,
+	name: 'Chessiano Pizza',
+	address: 'Opposite Pantaloons, Senapati Bapat Road, Pune',
+	location: 'Senapati Bapat Road',
+	category: 'Quick Bites',
+	vote: '3.3',
+	noofvotes: '460 Votes',
+	cuisines: 'Pizza',
+	url: 'Chessiano Pizza',
+	cost: '1800',
+	hours: '11 AM to 1 AM (Mon-Sun)',
+	email: 'chessinopizza@gmail.com',
+	image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSk_TLf0yvTv4vaYBT93zC0a-HX7O2c7DzybqlMvpDbSfQxPXrxtA'
+},
+hoteldata
+]						});
 
 
 // controller for url change when click on login button
@@ -281,7 +297,9 @@ foodieApp.controller('restaurantController',function($scope,$routeParams,$http) 
 												hours: '11 AM to 1 AM (Mon-Sun)',
 												email: 'chessinopizza@gmail.com',
 												image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSk_TLf0yvTv4vaYBT93zC0a-HX7O2c7DzybqlMvpDbSfQxPXrxtA'
-										}]
+										},
+										hoteldata
+									]
 
 	$scope.restaurant = restaurants[$routeParams.id - 1];
 
